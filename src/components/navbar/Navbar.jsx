228e523057140075f76609e88437768668061768
shxrox <sharondeva8@gmail.com>
2025-05-08 17:23:11 +0530

    Navbar Updating

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,6 @@ const Navbar = () => {
   return (
     <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
       <Link to="/" style={{ color: 'white', marginRight: '20px' }}>Home</Link>
-      <Link to="/favorites" style={{ color: 'white', marginRight: '20px' }}>Favorites</Link>
       <Link to="/trending" style={{ color: 'white', marginRight: '20px' }}>Trending</Link>
       <Link to="/popular" style={{ color: 'white', marginRight: '20px' }}>Popular</Link>
       <Link to="/top-rated" style={{ color: 'white' }}>Top Rated</Link>
