7f15d589d9092bd894e096a046079b0f0489ee89
shxrox <sharondeva8@gmail.com>
2025-05-08 17:28:14 +0530

    TopRated File

---
+++ src/pages/TopRated.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { fetchTopRatedMovies } from "../services/movieService";
+import { Link } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+
+const TopRated = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getTopRatedMovies = async () => {
+      const data = await fetchTopRatedMovies();
+      setMovies(data);
+    };
+
+    getTopRatedMovies();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <h2>Top Rated Movies</h2>
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <div key={movie.id} className="movie-item">
+            <Link to={`/movie/${movie.id}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={movie.title}
+                className="movie-poster"
+              />
+            </Link>
+            <div className="movie-details">
+              <Link to={`/movie/${movie.id}`}>
+                <h3>{movie.title}</h3>
+              </Link>
+              <p><strong>Release Date:</strong> {movie.release_date}</p>
+              <p><strong>Rating:</strong> {movie.vote_average}</p>
+              <p><strong>Overview:</strong> {movie.overview}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopRated;
